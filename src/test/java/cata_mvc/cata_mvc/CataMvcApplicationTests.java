e567e6964639fc5ab2d6e7e2f07649c10a33e9c8
abdalodj <lodianea98@gmail.com>
2022-03-23 09:18:30 +0000

    Initial commit

---
+++ src/test/java/cata_mvc/cata_mvc/CataMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package cata_mvc.cata_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CataMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
